cbe1b88e404638c349eda3b730f7ddc68ca3e154
ingandres00 <juliochaparro8706@gmail.com>
2022-04-22 13:27:20 -0500

    Add files via upload

---
+++ 02 - Variables y Tipos de Datos/Tarea.py
@@ -0,0 +1,100 @@
+#1
+mivariable1 = 1
+print("mivariable1")
+
+#2
+type = 8.5
+print("mivariable2")
+
+#3
+print(type("mivariable1"))
+
+#4
+ab= "Julio"
+
+#5
+mivariable3 = 9 + 4i
+
+#6
+print(type(mivariable3))
+
+#7
+mivariable4 = 3.1415
+print(round(pi,2))
+
+#8
+a = 'True'
+b = true
+
+var1 = '2.4'
+var2 = 4
+print (int(var1) + int(var2))
+
+#9
+print (type var1)
+print (type var2)
+
+#10
+mivariable5 = 5
+mivariable6 = 9.5
+mivariable7 = mivariable5 + mivariable6
+
+#11
+
+c1 = 9 +3j
+c2 = 3 + 13j
+print(c1 + c2)
+
+#12
+print(8+4j)
+
+#13
+mivariable8 = mivariable6 * mivariable7
+
+#14
+mivariable9 = 2**8
+print(mivariable9)
+
+#15
+mivariable10 = 27/4
+print(mivariable10)
+
+#16
+print(27 // 4)
+
+#17
+27 % 4
+
+#18
+6*4+3
+
+#19
+var3= 'Santa'
+var4= 'Cruz'
+print(var3 + var4)
+
+#20
+2 == '2'
+
+#21
+
+
+#22
+a = a.replace(',','.')
+a = float(a)
+
+#23
+a = -3
+a = a -1
+print(a)
+
+#24
+1 << 2
+
+#25
+2 + '2'
+
+#26
+var9 = 9
+var10 = '10'
+print (int(var9)+var10(10))
